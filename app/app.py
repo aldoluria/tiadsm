379cce7b3e8da1ab5192f75680ec5e77a9d862db
aldoluria <aldo.luria@gmail.com>
2024-06-20 11:28:35 -0600

    CRUD Alumnos v2.0

--- app/app.py
+++ app/app.py
@@ -3,8 +3,11 @@ import uuid
 import psycopg2
 from datetime import datetime
 from werkzeug.utils import secure_filename
-from flask import Flask, render_template, url_for, redirect, request, flash
+from flask import Flask, render_template, url_for, redirect, request, flash, Blueprint
 from flask_wtf.csrf import CSRFProtect
+
+#Creamos una tag con la ayuda de Blueprint y la iniciamos en nuestro proyecto (al crear nuestra applicación)
+custom_tags = Blueprint('custom_tags', __name__)
 
 app = Flask(__name__)
 csrf=CSRFProtect()
@@ -43,26 +46,37 @@ ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 ruta_alumnos=app.config['UPLOAD_FOLDER']='./app/static/img/uploads/alumnos/'
 ruta_profesores=app.config['UPLOAD_FOLDER']='./app/static/img/uploads/profesores/'
 
-app.secret_key='mysecretkey'
-
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+#@custom_tags.app_template_global()
+def listar_profesores():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM profesores WHERE activo=true ORDER BY nombre ASC;')
+    profesores = cur.fetchall()
+    cur.close()
+    conn.close()
+    return profesores
+
+app.secret_key='mysecretkey'
+
 @app.route("/")
 def index():
     titulo = "Plataforma de cursos"
-    return render_template('index.html', titulo=titulo)
+    return render_template('index.html', titulo=titulo, profesores=listar_profesores())
 
 @app.route("/about-us")
 def about_us():
-    return render_template('about_us.html')
+    return render_template('about_us.html', profesores=listar_profesores())
 
 @app.route("/dashboard")
 def dashboard():
     titulo = "Panel de Administración"
     return render_template('dashboard.html', titulo=titulo)
 
+#------------------------- DASHBOARD -------------------------
 #------------------------- CRUD Alumnos -------------------------
 
 @app.route("/dashboard/alumnos")
@@ -107,9 +121,9 @@ def alumnos_crear():
 
             conn = get_db_connection()
             cur = conn.cursor()
-            cur.execute('INSERT INTO alumnos (nombre, apellido_paterno, apellido_materno, activo, creado, editado, imagen, situacion)'
-                        'VALUES (%s, %s, %s, %s, %s, %s, %s, %s)',
-                        (nombre, paterno, materno, activo, creado, editado,filename,situacion))
+            sql="INSERT INTO alumnos (nombre, apellido_paterno, apellido_materno, activo, creado, editado, imagen, situacion) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+            valores=(nombre, paterno, materno, activo, creado, editado,filename,situacion)
+            cur.execute(sql,valores)
             conn.commit()
             cur.close()
             conn.close()
@@ -167,6 +181,51 @@ def alumnos_actualizar(id):
         flash('¡Alumno modificado exitosamente!')
     return redirect(url_for('alumnos_dashboard'))
 
+@app.route('/dashboard/alumnos/actualizar/foto/<string:id>', methods=['POST'])
+def alumnos_actualizar_foto(id):
+    if request.method == 'POST':
+        imagen=request.files['Foto']
+        nombre = request.form['nombre']
+        paterno = request.form['paterno']
+        materno = request.form['materno']
+        foto_anterior = request.form['anterior']
+        foto_anterior = os.path.join(ruta_alumnos,foto_anterior)
+        editado = datetime.now()
+
+        if imagen and allowed_file(imagen.filename):
+            # Verificar si el archivo con el mismo nombre ya existe
+            # Creamos un nombre dinamico para la foto de perfil con el nombre del alumno y una cadena aleatoria
+            cadena_aleatoria = my_random_string(10)
+            filename = paterno + "_" + materno + "_" + nombre + "_" + str(editado)[:10] + "_" + cadena_aleatoria + "_" + secure_filename(imagen.filename)
+            file_path = os.path.join(ruta_alumnos, filename)
+            if os.path.exists(file_path):
+                flash('Error: ¡Un archivo con el mismo nombre ya existe! Intente renombrar su archivo.')
+                return redirect(url_for('alumnos_dashboard'))
+            # Guardar el archivo y registrar en la base de datos
+            imagen.save(file_path)
+
+            conn = get_db_connection()
+            cur = conn.cursor()
+            sql=" UPDATE alumnos SET editado=%s, imagen=%s WHERE id_alumno=%s"
+            valores=(editado,filename,id)
+            cur.execute(sql,valores)
+            conn.commit()
+            cur.close()
+            conn.close()
+            
+            #Eliminar foto de perfil antigua
+            if request.form['anterior'] != "":
+                if os.path.exists(foto_anterior):
+                    os.remove(foto_anterior)
+
+            flash('¡Foto de perfil actualizada exitosamente!')
+            return redirect(url_for('alumnos_editar', id=id))
+        else:
+            flash('Error: ¡Extensión de archivo invalida! Intente con una imagen valida PNG, JPG o JPEG')
+            return redirect(url_for('alumnos_editar', id=id))
+
+    return redirect(url_for('alumnos_dashboard'))
+
 @app.route('/dashboard/alumnos/eliminar/<string:id>')
 def alumnos_eliminar(id):
     activo = False
@@ -183,6 +242,31 @@ def alumnos_eliminar(id):
     flash('¡Alumno  eliminado correctamente!')
     return redirect(url_for('alumnos_dashboard'))
 
+@app.route('/dashboard/alumnos/eliminar/foto/<string:foto>/<string:id>')
+def alumnos_eliminar_foto(foto,id):
+    foto_anterior = os.path.join(ruta_alumnos,foto)
+    editado = datetime.now()
+    conn = get_db_connection()
+    cur = conn.cursor()
+    #sql="DELETE FROM alumnos WHERE id_alumno={0}".format(id)
+    sql="UPDATE alumnos SET imagen=%s, editado=%s WHERE id_alumno=%s"
+    valores=("",editado,id)
+    cur.execute(sql,valores)
+    conn.commit()
+    cur.close()
+    conn.close()
+    #Eliminar foto de perfil antigua
+    print(foto_anterior)
+    if foto != "":
+        if os.path.exists(foto_anterior):
+            os.remove(foto_anterior)
+            flash('¡Foto eliminada correctamente!')
+            return redirect(url_for('alumnos_editar', id=id))
+    else:
+        flash('Error: ¡No se puede ejecutar esta acción!')
+        return redirect(url_for('alumnos_editar', id=id))
+
+
 #------------------------- CRUD Profesores -------------------------
 
 #------------------------- CRUD Materias -------------------------
@@ -196,6 +280,7 @@ def acceso_no_autorizado(error):
 
 if __name__ == '__main__':
     csrf.init_app(app)
+    app.register_blueprint(custom_tags)
     app.register_error_handler(404, pagina_no_encontrada)
     app.register_error_handler(401, acceso_no_autorizado)
     app.run(debug=True, port=5000)
